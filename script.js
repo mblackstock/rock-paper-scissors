c44d3040405994ced7671f28267ddbbde830c23f
mblackstock <mikeblackstock@gmail.com>
2023-05-30 21:47:59 +0000

    add computer choice

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let user_choice ="";
 let computer_choice = "";
 
+let user_choice_element = document.getElementById("user_choice");
+let computer_choice_element = document.getElementById("computer_choice");
+
 let buttons = document.querySelectorAll(".choice");
 
 buttons.forEach(e => {
@@ -10,6 +13,16 @@ buttons.forEach(e => {
 function handleChoiceClick(e) {
   let button = e.target;
   let choice = button.dataset.choice;
-  
-  console.log(`click ${choice}`);
+
+  user_choice_element.innerText = choice;
+
+  let computerChoice = generateComputerChoice();
+
+  computer_choice_element.innerText = computerChoice;
+}
+
+function generateComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const choiceNum = Math.floor(Math.random()*3);
+  return choices[choiceNum];
 }
